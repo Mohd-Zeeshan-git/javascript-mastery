4b710273d6e3b1a649c7f89452334781a5352307
Mohd-Zeeshan-git <mohdzeeshanhasan06@gmail.com>
2026-01-10 13:34:02 +0000

    Add HTML and JavaScript files for the Unlimited Color project with background color changing functionality

---
+++ 05_projects/06_unlimitedColor/index.js
@@ -0,0 +1,34 @@
+const randomColor=function(){
+    const hex= "123456789ABCDEF"
+    let color="#"
+    for(let i=0;i<6;i++){
+        // Math.floor(Math.random()*16)//this will give sixteen random number
+        color += hex[Math.floor(Math.random()*16)] //this will give the position of hex
+        
+    }
+    return color;
+}
+
+
+let intervalId;
+const startChangingColor= function(){
+
+    function changeBgColor(){
+        document.body.style.backgroundColor= randomColor();
+        // console.log("changed");
+        
+    }
+
+  intervalId = setInterval(changeBgColor,2000);
+}
+const stopChangingColor= function(){
+clearInterval(intervalId);
+intervalId= null;// flush out intervalId 
+// console.log("stoped")
+
+}
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
+
+
